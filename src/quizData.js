af73bf10081d341f1c08e2da742a0a841ec2f415
Mubeen jawed <mubeenjawed3@gmail.com>
2023-07-13 11:46:09 +0500

    Initial commit

---
+++ src/quizData.js
@@ -0,0 +1,233 @@
+import React from "react"
+
+function quizData() {
+  return (
+
+    [
+      {
+        "category": "music",
+        "id": "1",
+        "correctAnswer": "The Carpenters",
+        "incorrectAnswers": [
+          "The Carpenters",
+          "The Black Keys",
+          "Three 6 Mafia",
+          "The Velvet Underground",
+        ],
+        "question": {
+          "text": "Which American vocal duo released the studio album 'A Song for You'?"
+        },
+        "tags": [
+          "music"
+        ],
+        "type": "text_choice",
+        "difficulty": "hard",
+        "regions": [],
+        "isNiche": false
+      },
+      {
+        "category": "arts_and_literature",
+        "id": "2",
+        "correctAnswer": "High Renaissance",
+        "incorrectAnswers": [
+          "Baroque",
+          "Impressionism",
+          "High Renaissance",
+          "Mannerism",
+        ],
+        "question": {
+          "text": "The painting The Triumph of Galatea by Raphael is a part of which art movement?"
+        },
+        "tags": [
+          "art",
+          "painting"
+        ],
+        "type": "text_choice",
+        "difficulty": "hard",
+        "regions": [],
+        "isNiche": false
+      },
+      {
+        "category": "general_knowledge",
+        "id": "3",
+        "correctAnswer": "92",
+        "incorrectAnswers": [
+          "92",
+          "90",
+          "89",
+          "91",
+        ],
+        "question": {
+          "text": "What is the total number of days in March, April, and May combined?"
+        },
+        "tags": [
+          "numbers",
+          "arithmetic"
+        ],
+        "type": "text_choice",
+        "difficulty": "medium",
+        "regions": [],
+        "isNiche": false
+      },
+      {
+        "category": "film_and_tv",
+        "id": "4",
+        "correctAnswer": "Jaws",
+        "incorrectAnswers": [
+          "Jaws",
+          "The Silence of the Lambs",
+          "The Good, the Bad and the Ugly",
+          "Platoon",
+        ],
+        "question": {
+          "text": "Name the movie that matches the following plot summary: 'A killer shark unleashes chaos on a beach community.'"
+        },
+        "tags": [
+          "film",
+          "film_and_tv"
+        ],
+        "type": "text_choice",
+        "difficulty": "easy",
+        "regions": [],
+        "isNiche": false
+      },
+      {
+        "category": "general_knowledge",
+        "id": "5",
+        "correctAnswer": "Silver",
+        "incorrectAnswers": [
+          "Gold",
+          "Silver",
+          "Iron",
+          "Copper",
+        ],
+        "question": {
+          "text": "What metal corresponds to the Spanish term 'plata'?"
+        },
+        "tags": [
+          "spanish"
+        ],
+        "type": "text_choice",
+        "difficulty": "hard",
+        "regions": [],
+        "isNiche": false
+      },
+      {
+        "category": "society_and_culture",
+        "id": "6",
+        "correctAnswer": "Military and political leader of the Mongols.",
+        "incorrectAnswers": [
+          "Engineer who develop radio transmission.",
+          "Military and political leader of the Mongols.",
+          "Political philosopher who promoted a theory.",
+          "Inventor of the printing press.",
+        ],
+        "question": {
+          "text": "Which of the following describes Genghis Kahn?"
+        },
+        "tags": [
+          "people",
+          "history",
+          "society_and_culture"
+        ],
+        "type": "text_choice",
+        "difficulty": "easy",
+        "regions": [],
+        "isNiche": false
+      },
+
+      {
+        "category": "film_and_tv",
+        "id": "7",
+        "correctAnswer": "Renée Zellweger",
+        "incorrectAnswers": [
+          "Shohreh Aghdashloo",
+          "Renée Zellweger",
+          "Patricia Clarkson",
+          "Marcia Gay Harden"
+        ],
+        "question": {
+          "text": "Who won the 2003 Academy Award for Best Supporting Actress for playing the role of Ruby Thewes in Cold Mountain?"
+        },
+        "tags": [
+          "acting",
+          "film",
+          "academy_awards",
+          "film_and_tv"
+        ],
+        "type": "text_choice",
+        "difficulty": "hard",
+        "regions": [],
+        "isNiche": false
+      },
+      {
+        "category": "arts_and_literature",
+        "id": "8",
+        "correctAnswer": "Macduff",
+        "incorrectAnswers": [
+          "Macburn",
+          "Macduff",
+          "Lady Macbeth",
+          "Duncan"
+        ],
+        "question": {
+          "text": "Who killed Macbeth?"
+        },
+        "tags": [
+          "arts_and_literature"
+        ],
+        "type": "text_choice",
+        "difficulty": "medium",
+        "regions": [],
+        "isNiche": false
+      },
+      {
+        "category": "society_and_culture",
+        "id": "9",
+        "correctAnswer": "Oak",
+        "incorrectAnswers": [
+          "Pine",
+          "Cedar",
+          "Walnut",
+          "Oak",
+        ],
+        "question": {
+          "text": "Which tree do Druids regard sacred ?"
+        },
+        "tags": [
+          "society_and_culture"
+        ],
+        "type": "text_choice",
+        "difficulty": "hard",
+        "regions": [],
+        "isNiche": false
+      },
+      {
+        "category": "music",
+        "id": "10",
+        "correctAnswer": "Aaron",
+        "incorrectAnswers": [
+          "Aaron",
+          "Romeo",
+          "Dylan",
+          "Zachary"
+        ],
+        "question": {
+          "text": "What Was Elvis Presley's Middle Name?"
+        },
+        "tags": [
+          "names",
+          "people",
+          "music"
+        ],
+        "type": "text_choice",
+        "difficulty": "hard",
+        "regions": [],
+        "isNiche": false
+      },
+    ]
+  )
+}
+
+export default quizData
+
