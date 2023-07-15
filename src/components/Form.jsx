32f12a3122a6522501f898405a205283bc51645d
Mubeen jawed <mubeenjawed3@gmail.com>
2023-07-15 14:18:35 +0500

    Initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom';
+
+const Form = () => {
+
+  const [quizName, setQuizName] = useState("")
+  const [questionNumber, setQuestionNumber] = useState(0)
+  const [question, setQuestion] = useState("")
+  const [option1, setOption1] = useState("")
+  const [option2, setoption2] = useState("")
+  const [option3, setoption3] = useState("")
+  const [option4, setoption4] = useState("")
+  const [answer, setAnswer] = useState("")
+
+  function handleQuizClick() {
+    axios.post("http://localhost:8080/createQuiz", {
+      questionNumber, question, option1, option2, option3, option4, answer, quizName
+    })
+
+    localStorage.setItem("userQuizName", quizName)
+  }
+
+  function handleAddQuestionClick() {
+    axios.post("http://localhost:8080/addQuestion", {
+      questionNumber, question, option1, option2, option3, option4, answer, quizName
+    })
+  }
+
+
+  return (
+    <div>
+      <form>
+        <div class="group">
+          <input onChange={(e) => { setQuizName(e.target.value) }} type="text" /><span class="highlight"></span><span class="bar"></span>
+          <label>Quiz Name</label>
+        </div>
+        <div class="group">
+          <input onChange={(e) => setQuestionNumber(e.target.value)} type="number" /><span class="highlight"></span><span class="bar"></span>
+          <label>Question #</label>
+        </div>
+        <div class="group">
+          <input onChange={(e) => setQuestion(e.target.value)} type="text" /><span class="highlight"></span><span class="bar"></span>
+          <label>Question</label>
+        </div>
+        <div class="group">
+          <input onChange={(e) => setOption1(e.target.value)} type="text" /><span class="highlight"></span><span class="bar"></span>
+          <label>Option 1</label>
+        </div>
+        <div class="group">
+          <input onChange={(e) => setoption2(e.target.value)} type="text" /><span class="highlight"></span><span class="bar"></span>
+          <label>Option 2</label>
+        </div>
+        <div class="group">
+          <input onChange={(e) => setoption3(e.target.value)} type="text" /><span class="highlight"></span><span class="bar"></span>
+          <label>Option 3</label>
+        </div>
+        <div class="group">
+          <input onChange={(e) => setoption4(e.target.value)} type="text" /><span class="highlight"></span><span class="bar"></span>
+          <label >Option 4</label>
+        </div>
+        <div class="group">
+          <input onChange={(e) => setAnswer(e.target.value)} type="text" /><span class="highlight"></span><span class="bar"></span>
+          <label>Correct Answer</label>
+        </div>
+        <button onClick={handleAddQuestionClick} type="submit" class="button buttonBlue">Add another Question
+          <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+        </button>
+        <Link to="/" onClick={handleQuizClick} type='submit' class="button buttonRed">Create Quiz
+          <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+        </Link>
+      </form>
+    </div>
+  )
+}
+
+export default Form
